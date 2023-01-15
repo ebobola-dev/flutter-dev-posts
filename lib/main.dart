d13f50977af0761fcb17857e2222e33c5982da97
ebobola-web <romawot94@gmail.com>
2023-01-15 11:47:17 +0300

    added fonts, initial project setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
+    return const MaterialApp(
+      title: 'Flutter Dev Posts',
+      home: Scaffold(),
     );
   }
 }

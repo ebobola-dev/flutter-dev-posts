02eb0725143578469e5944eace0801c4d76b3821
ebobola-web <romawot94@gmail.com>
2023-01-16 20:52:59 +0300

    made Readme

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
   //? Фейковая задержка при инициализации (чтении постов из бд) и их обновлении
-  static const bool fakeDelay = true;
+  static const bool fakeDelay = false;
   static const int fakeDelaySeconds = 3;
 }
